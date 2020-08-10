dc860358b5a1330627e399f0c822f32b4dcf2d5f
Nickanda <songyang20092009@gmail.com>
2020-08-09 22:37:50 -0400

    Update Client.js

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -1,5 +1,167 @@
 const { Client } = require('discord.js');
 
 module.exports = class DiscordClient extends Client {
+    constructor(options) {
+        super(options);
+
+        // Here we load the config.js file that contains our token and our prefix values.
+        this.config = require("../../config.json");
+        // client.config.token contains the bot's token
+        // client.config.prefix contains the message prefix
+
+        // Aliases and commands are put in collections where they can be read from,
+        // catalogued, listed, etc.
+        this.commands = new Collection();
+        this.aliases = new Collection();
+
+        // Now we integrate the use of Evie's awesome Enhanced Map module, which
+        // essentially saves a collection to disk. This is great for per-server configs,
+        // and makes things extremely easy for this purpose.
+        this.settings = new Enmap({ name: "settings", cloneLevel: "deep", fetchAll: false, autoFetch: true });
+
+        //requiring the Logger class for easy console logging
+        this.logger = require("./modules/Logger");
+
+        // Basically just an async shortcut to using a setTimeout. Nothing fancy!
+        this.wait = require("util").promisify(setTimeout);
+    }
+
+    /*
+    PERMISSION LEVEL FUNCTION
+    This is a very basic permission system for commands which uses "levels"
+    "spaces" are intentionally left black so you can add them if you want.
+    NEVER GIVE ANYONE BUT OWNER THE LEVEL 10! By default this can run any
+    command including the VERY DANGEROUS `eval` command!
+    */
+    permlevel(message) {
+        let permlvl = 0;
+
+        const permOrder = this.config.permLevels.slice(0).sort((p, c) => p.level < c.level ? 1 : -1);
+
+        while (permOrder.length) {
+            const currentLevel = permOrder.shift();
+            if (message.guild && currentLevel.guildOnly) continue;
+            if (currentLevel.check(message)) {
+                permlvl = currentLevel.level;
+                break;
+            }
+        }
+        return permlvl;
+    }
+
+    /* 
+    COMMAND LOAD AND UNLOAD
     
+    To simplify the loading and unloading of commands from multiple locations
+    including the index.js load loop, and the reload function, these 2 ensure
+    that unloading happens in a consistent manner across the board.
+    */
+
+    loadCommand(commandPath, commandName) {
+        try {
+            const props = new (require(`${commandPath}${path.sep}${commandName}`))(this);
+            this.logger.log(`Loading Command: ${props.help.name}. 👌`, "log");
+            props.conf.location = commandPath;
+            if (props.init) {
+                props.init(this);
+            }
+            this.commands.set(props.help.name, props);
+            props.conf.aliases.forEach(alias => {
+                this.aliases.set(alias, props.help.name);
+            });
+            return false;
+        } catch (e) {
+            return `Unable to load command ${commandName}: ${e}`;
+        }
+    }
+
+    async unloadCommand(commandPath, commandName) {
+        let command;
+        if (this.commands.has(commandName)) {
+            command = this.commands.get(commandName);
+        } else if (this.aliases.has(commandName)) {
+            command = this.commands.get(this.aliases.get(commandName));
+        }
+        if (!command) return `The command \`${commandName}\` doesn"t seem to exist, nor is it an alias. Try again!`;
+
+        if (command.shutdown) {
+            await command.shutdown(this);
+        }
+        delete require.cache[require.resolve(`${commandPath}${path.sep}${commandName}.js`)];
+        return false;
+    }
+
+    /*
+    MESSAGE CLEAN FUNCTION
+    "Clean" removes @everyone pings, as well as tokens, and makes code blocks
+    escaped so they're shown more easily. As a bonus it resolves promises
+    and stringifies objects!
+    This is mostly only used by the Eval and Exec commands.
+    */
+    async clean(text) {
+        if (text && text.constructor.name == "Promise")
+            text = await text;
+        if (typeof text !== "string")
+            text = require("util").inspect(text, { depth: 1 });
+
+        text = text
+            .replace(/`/g, "`" + String.fromCharCode(8203))
+            .replace(/@/g, "@" + String.fromCharCode(8203))
+            .replace(this.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+
+        return text;
+    }
+
+    /* SETTINGS FUNCTIONS
+    These functions are used by any and all location in the bot that wants to either
+    read the current *complete* guild settings (default + overrides, merged) or that
+    wants to change settings for a specific guild.
+    */
+
+    // getSettings merges the client defaults with the guild settings. guild settings in
+    // enmap should only have *unique* overrides that are different from defaults.
+    getSettings(guild) {
+        const defaults = this.settings.get("default") || {};
+        const guildData = guild ? this.settings.get(guild.id) || {} : {};
+        const returnObject = {};
+        Object.keys(defaults).forEach((key) => {
+            returnObject[key] = guildData[key] ? guildData[key] : defaults[key];
+        });
+        return returnObject;
+    }
+
+    // writeSettings overrides, or adds, any configuration item that is different
+    // than the defaults. This ensures less storage wasted and to detect overrides.
+    writeSettings(id, newSettings) {
+        const defaults = this.settings.get("default");
+        let settings = this.settings.get(id);
+        if (typeof settings != "object") settings = {};
+        for (const key in newSettings) {
+            if (defaults[key] !== newSettings[key]) {
+                settings[key] = newSettings[key];
+            } else {
+                delete settings[key];
+            }
+        }
+        this.settings.set(id, settings);
+    }
+
+    /*
+    SINGLE-LINE AWAITMESSAGE
+    A simple way to grab a single reply, from the user that initiated
+    the command. Useful to get "precisions" on certain things...
+    USAGE
+    const response = await client.awaitReply(msg, "Favourite Color?");
+    msg.reply(`Oh, I really love ${response} too!`);
+    */
+    async awaitReply(msg, question, limit = 60000) {
+        const filter = m => m.author.id === msg.author.id;
+        await msg.channel.send(question);
+        try {
+            const collected = await msg.channel.awaitMessages(filter, { max: 1, time: limit, errors: ["time"] });
+            return collected.first().content;
+        } catch (e) {
+            return false;
+        }
+    }
 }
