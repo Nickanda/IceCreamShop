ebaa349abb75a14f5adb21bad68318b84a1bd301
Nickanda <songyang20092009@gmail.com>
2020-12-15 11:09:22 -0500

    update invite link

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -15,7 +15,7 @@ module.exports = class InviteCommand extends Command {
     async run(message, args) {
         const embed = new Discord.MessageEmbed()
             .setTitle("Ice Cream Shop")
-            .addField("Invite Link", "[Invite Me!](https://discord.com/oauth2/authorize?client_id=765627044687249439&scope=bot&permissions=8)", true)
+            .addField("Invite Link", "[Invite Me!](https://discord.com/oauth2/authorize?client_id=765627044687249439&scope=bot&permissions=347200)", true)
             .setColor(0x00FF00)
             .setThumbnail(this.client.user.displayAvatarURL())
             .setFooter('i!help', this.client.user.displayAvatarURL())
--- src/commands/support.js
+++ src/commands/support.js
@@ -16,7 +16,7 @@ module.exports = class SupportCommand extends Command {
         const embed = new Discord.MessageEmbed()
             .setTitle("Ice Cream Shop")
             .addField("Support Server", "https://discord.gg/sXkpG2J", true)
-            .addField("Invite Link", "[Invite Me!](https://discord.com/oauth2/authorize?client_id=765627044687249439&scope=bot&permissions=8)", true)
+            .addField("Invite Link", "[Invite Me!](https://discord.com/oauth2/authorize?client_id=765627044687249439&scope=bot&permissions=347200)", true)
             .setColor(0x00FF00)
             .setThumbnail(this.client.user.displayAvatarURL())
             .setFooter('i!help', this.client.user.displayAvatarURL())
