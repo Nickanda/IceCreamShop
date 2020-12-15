01d99b6533aa3a541e6f94816c4b6ccba08ed8b8
Nickanda <songyang20092009@gmail.com>
2020-12-15 16:50:51 -0500

    machines command

--- src/commands/machines.js
+++ src/commands/machines.js
@@ -15,10 +15,15 @@ module.exports = class MachinesCommand extends Command {
     async run(message, args) {
         const profile = await this.client.shopHandler.getProfile(message);
 
+        let machineCap = "";
+        for (const [key, val] of Object.entries(capacity)) {
+            machineCap += `\n${val.type} Machine ${key}: ${val.capacity}%`
+        }
+
         const embed = new Discord.MessageEmbed()
             .setAuthor(message.author.tag, message.author.displayAvatarURL())
             .setTitle(profile.get('name'))
-            .setDescription(`💰 $${profile.get('money')}`)
+            .setDescription(`Your machines:${machineCap}`)
             .setColor(0x00FF00)
             .setFooter('i!help', this.client.user.displayAvatarURL())
             .setTimestamp();
