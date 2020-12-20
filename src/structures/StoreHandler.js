99fad2a9e19809a16659e01db9a3644ffe3dcaaa
Nickanda <songyang20092009@gmail.com>
2020-12-20 01:10:03 +0000

    id system

--- src/structures/StoreHandler.js
+++ src/structures/StoreHandler.js
@@ -4,58 +4,72 @@ module.exports = class StoreHandler {
 
         this.ads = {
             "Newspaper": {
+                id: "A01",
                 cost: 200,
                 duration: 1000 * 60 * 60 * 4,
                 boost: 1.1
             },
             "Radio": {
+                id: "A02",
                 cost: 350,
                 duration: 1000 * 60 * 60 * 6,
                 boost: 1.15
             },
             "TV": {
+                id: "A03",
                 cost: 500,
                 duration: 1000 * 60 * 60 * 12,
                 boost: 1.25
             },
             "Google": {
+                id: "A04",
                 cost: 1000,
                 duration: 1000 * 60 * 60 * 18,
                 boost: 1.5
             },
             "Billboard": {
+                id: "A05",
                 cost: 2500,
                 duration: 1000 * 60 * 60 * 24,
                 boost: 2
             },
             "Airplane": {
+                id: "A06",
                 cost: 5000,
                 duration: 1000 * 60 * 60 * 8,
-                boost: 4
+                boost: 2
             },
             "YouTube": {
-
+                id: "A07",
+                cost: 10000,
+                duration: 1000 * 60 * 60 * 12,
+                boost: 5
             }
         };
 
         this.flavors = {
             "vanilla": {
+                id: "F01",
                 cost: 0,
                 boost: 1
             },
             "chocolate": {
+                id: "F02",
                 cost: 50,
                 boost: 1.05
             },
             "strawberry": {
+                id: "F03",
                 cost: 50,
                 boost: 1.05
             },
             "chocolate chip": {
+                id: "F04",
                 cost: 100,
                 boost: 1.1
             },
             "cookie & cream": {
+                id: "F05",
                 cost: 100,
                 boost: 1.1
             }
@@ -63,18 +77,22 @@ module.exports = class StoreHandler {
 
         this.machines = {
             "Basic": {
+                id: "M01",
                 cost: 2500,
                 boost: 1
             },
             "Regular": {
+                id: "M02",
                 cost: 10000,
                 boost: 1.25
             },
             "Improved": {
+                id: "M03",
                 cost: 25000,
                 boost: 1.5
             },
             "Advanced": {
+                id: "M04",
                 cost: 50000,
                 boost: 2
             }
