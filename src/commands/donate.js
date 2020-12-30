159aff6d01fc1aa84b48ec0feb113d8e15ce024f
Nickanda <songyang20092009@gmail.com>
2020-12-30 02:24:58 +0000

    add donation link

--- src/commands/donate.js
+++ src/commands/donate.js
@@ -16,6 +16,7 @@ module.exports = class DonateCommand extends Command {
         const embed = new Discord.MessageEmbed()
             .setTitle("Ice Cream Shop")
             .addField("Developer", "NicholasY#4815", true)
+            .addField("Donate", "[Click here!](https://www.buymeacoffee.com/NicholasY4815)", true)
             .setColor(0x00FF00)
             .setThumbnail(this.client.user.displayAvatarURL())
             .setFooter('i!help', this.client.user.displayAvatarURL())
