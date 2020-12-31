80a4a734c6433e12fdb8953f1eacb47eecb20553
Nickanda <songyang20092009@gmail.com>
2020-12-31 16:24:42 +0000

    flavors command

---
+++ src/commands/flavors.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+const Command = require('../structures/Command');
+
+module.exports = class MachinesCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "flavors",
+            description: "Shows the flavors that you currently have",
+            category: "Shop",
+            usage: "flavors"
+        });
+    }
+
+    async run(message, args) {
+        const profile = await this.client.shopHandler.getProfile(message);
+
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL())
+            .setTitle(profile.get('name'))
+            .setDescription(`Your flavors: ${JSON.parse(profile.get('flavors')).join(', ')}`)
+            .setColor(0x00FF00)
+            .setFooter('i!help', this.client.user.displayAvatarURL())
+            .setTimestamp();
+
+        message.channel.send(embed);
+    }
+}
