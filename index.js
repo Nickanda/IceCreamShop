d5f1812943473c0101c44647baa04d7ee9ebdfcf
Nickanda <songyang20092009@gmail.com>
2021-01-21 02:15:52 +0000

    edit caching behavior

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const Discord = require('discord.js');
 
 const Client = require('./src/structures/Client');
 
-const client = new Client({ fetchAllMembers: true, ws: { intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] } });
+const client = new Client({
+    fetchAllMembers: true,
+    messageCacheMaxSize: 20,
+    messageCacheLifetime: 120,
+    messageSweepInterval: 120,
+    messageEditHistoryMaxSize: 1,
+    ws: {
+        intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]
+    } 
+});
 
 const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
