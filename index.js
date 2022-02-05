5609b331bae497d9010da305f288ac9e7c4cb8ac
NicholasY4815 <songyang20092009@gmail.com>
2022-02-05 06:51:08 -0500

    update to 13.6

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const express = require('express');
 const Client = require('./src/structures/Client');
 
 const client = new Client({
-  // messageCacheMaxSize: 20,
-  // messageCacheLifetime: 120,
-  // messageSweepInterval: 120,
-  // messageEditHistoryMaxSize: 1,
-  intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.DIRECT_MESSAGES]
 });
 
 const Sentry = require("@sentry/node");
